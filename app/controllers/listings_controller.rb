7f220f839827446d3c276d209b88f19a2972c7a2
Ishaan Sharma <Unkn0wn@Ishaan.local>
2016-05-22 21:59:50 +0530

    transfer funds to seller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,14 +1,12 @@
-
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:seller, :new, :create, :edit, :update, :destroy]
-  before_action :check_user, only: [:edit, :update, :destroy]
-
-
+  before_filter :authenticate_user!, only: [:seller, :new, :create, :edit, :update, :destroy]
+  before_filter :check_user, only: [:edit, :update, :destroy]
 
   def seller
     @listings = Listing.where(user: current_user).order("created_at DESC")
   end
+
   # GET /listings
   # GET /listings.json
   def index
@@ -35,12 +33,26 @@ class ListingsController < ApplicationController
     @listing = Listing.new(listing_params)
     @listing.user_id = current_user.id
 
+    if current_user.recipient.blank?
+      Stripe.api_key = ENV["STRIPE_API_KEY"]
+      token = params[:stripeToken]
+
+      recipient = Stripe::Recipient.create(
+        :name => current_user.name,
+        :type => "individual",
+        :bank_account => token
+        )
+
+      current_user.recipient = recipient.id
+      current_user.save
+    end
+
     respond_to do |format|
       if @listing.save
         format.html { redirect_to @listing, notice: 'Listing was successfully created.' }
-        format.json { render :show, status: :created, location: @listing }
+        format.json { render action: 'show', status: :created, location: @listing }
       else
-        format.html { render :new }
+        format.html { render action: 'new' }
         format.json { render json: @listing.errors, status: :unprocessable_entity }
       end
     end
@@ -52,9 +64,9 @@ class ListingsController < ApplicationController
     respond_to do |format|
       if @listing.update(listing_params)
         format.html { redirect_to @listing, notice: 'Listing was successfully updated.' }
-        format.json { render :show, status: :ok, location: @listing }
+        format.json { head :no_content }
       else
-        format.html { render :edit }
+        format.html { render action: 'edit' }
         format.json { render json: @listing.errors, status: :unprocessable_entity }
       end
     end
@@ -65,7 +77,7 @@ class ListingsController < ApplicationController
   def destroy
     @listing.destroy
     respond_to do |format|
-      format.html { redirect_to listings_url, notice: 'Listing was successfully destroyed.' }
+      format.html { redirect_to listings_url }
       format.json { head :no_content }
     end
   end
